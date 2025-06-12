a499e956991189dc34df9a513dec7a3c5be3a4b2
Lindsay Kowalick <lpkowalick@gmail.com>
2025-06-12 17:00:57 -0400

    whoops, forgot prompt

--- app/main.py
+++ app/main.py
@@ -2,12 +2,8 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-
-    # Wait for user input
     while True:
-        command = input()
+        command = input("$ ")
         print(f'{command}: command not found')
 
 
