4f6e6f526245cc7b2290b7040e9bc680712d9257
Lindsay Kowalick <lpkowalick@gmail.com>
2025-06-12 17:13:40 -0400

    echo

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import sys
 
 def main():
     while True:
-        command = input("$ ").split()
-        match command:
+        command_str = input("$ ")
+        match command_str.split():
             case ["exit", status]:
                 sys.exit(0)
+            case ["echo", *rest]:
+                sys.stdout(" ".join(rest))
             case _:
-                print(f'{" ".join(command)}: command not found')
+                print(f'{" ".join(command_str)}: command not found')
 
 if __name__ == "__main__":
     main()
