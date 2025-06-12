2c88bab54844017916af8e5f9d8acf07b514e37d
Lindsay Kowalick <lpkowalick@gmail.com>
2025-06-12 17:39:18 -0400

    fix multiple times in path

--- app/main.py
+++ app/main.py
@@ -15,15 +15,17 @@ def main():
                 if arg in COMMANDS:
                     print(f'{arg} is a shell builtin')
                 else:
-                    found = False
-                    for dir in os.environ["PATH"].split(":"):
-                        if not os.path.exists(dir):
-                            continue
-                        for executable in os.listdir(dir):
-                            if executable == arg:
-                                print(f'{arg} is {dir}/{executable}')
-                                found = True
-                    if not found:
+                    try:
+                        for dir in os.environ["PATH"].split(":"):
+                            if not os.path.exists(dir):
+                                continue
+                            for executable in os.listdir(dir):
+                                if executable == arg:
+                                    print(f'{arg} is {dir}/{executable}')
+                                    raise ValueError
+                    except ValueError:
+                        pass
+                    else:
                         print(f'{arg}: not found')
             case _:
                 print(f'{" ".join(command)}: command not found')
