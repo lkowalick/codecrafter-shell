971ac87fc98c1885217102d6cc711a213bfaebd2
Lindsay Kowalick <lpkowalick@gmail.com>
2025-06-13 14:14:11 -0400

    std out redirect

--- app/main.py
+++ app/main.py
@@ -11,33 +11,41 @@ SPACE = " "
 NEWLINE = "\n"
 
 def main():
-    while True:
-        full_command = tokenize(input("$ "))
-        match full_command:
-            case ["exit", status]:
-                sys.exit(0)
-            case ["echo", *rest]:
-                print(" ".join(rest))
-            case ["type", arg] if arg in BUILTINS:
-                print(f'{arg} is a shell builtin')
-            case ["type", arg]:
-                executable = find_executable(arg)
-                if executable:
-                    print(f'{arg} is {executable}')
-                else:
-                    print(f'{arg}: not found')
-            case ["pwd"]:
-                print(os.getcwd())
-            case ["cd", "~"]:
-                os.chdir(os.environ["HOME"])
-            case ["cd", destination] if os.path.exists(destination):
-                os.chdir(destination)
-            case ["cd", nonexistent_destination]:
-                    print(f'cd: {nonexistent_destination}: No such file or directory')
-            case [command, *args] if find_executable(command):
-                subprocess.run([command]+args)
-            case _:
-                print(f'{" ".join(full_command)}: command not found')
+    try:
+        while True:
+            full_command = tokenize(input("$ "))
+            output = sys.stdout
+            match full_command:
+                case [*beginning, ">", filename] | [*beginning, "1>", filename]:
+                    output = open(filename,'w')
+                    full_command = beginning
+            match full_command:
+                case ["exit", status]:
+                    sys.exit(0)
+                case ["echo", *rest]:
+                    output.write(" ".join(rest)+"\n")
+                case ["type", arg] if arg in BUILTINS:
+                    output.write(f'{arg} is a shell builtin\n')
+                case ["type", arg]:
+                    executable = find_executable(arg)
+                    if executable:
+                        output.write(f'{arg} is {executable}\n')
+                    else:
+                        output.write(f'{arg}: not found\n')
+                case ["pwd"]:
+                    output.write(os.getcwd())
+                case ["cd", "~"]:
+                    os.chdir(os.environ["HOME"])
+                case ["cd", destination] if os.path.exists(destination):
+                    os.chdir(destination)
+                case ["cd", nonexistent_destination]:
+                        output.write(f'cd: {nonexistent_destination}: No such file or directory\n')
+                case [command, *args] if find_executable(command):
+                    subprocess.run([command]+args)
+                case _:
+                    output.write(f'{" ".join(full_command)}: command not found\n')
+    finally:
+        output.close()
 
 def tokenize(string):
     i = 0
